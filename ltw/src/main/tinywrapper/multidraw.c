72ee6ce9e946d7c1617baa90af34f646607216e5
Adisorn-sudo <taqwar081@outlook.co.th>
2026-04-05 13:13:31 +0700

    Improve buffer binding checks in multidraw functions

--- ltw/src/main/tinywrapper/multidraw.c
+++ ltw/src/main/tinywrapper/multidraw.c
@@ -6,11 +6,20 @@
 
 #include <proc.h>
 #include <egl.h>
+#include "main.h"
 #include "basevertex.h"
+
+static inline void bind_buffer_if_needed(GLenum target, GLuint buffer) {
+    int index = get_buffer_index(target);
+    if(index != -1 && current_context->bound_buffers[index] == buffer) return;
+    es3_functions.glBindBuffer(target, buffer);
+    if(index != -1) current_context->bound_buffers[index] = buffer;
+}
+
 void glMultiDrawArrays( GLenum mode, GLint *first, GLsizei *count, GLsizei primcount )
 {
     // We'd need to merge each buffer attached to the VBO to properly achieve this. Nuh-uh. Aint no way im doin allat
-    if(!current_context) return;
+    if(!current_context || primcount == 0) return;
     for (int i = 0; i < primcount; i++) {
         if (count[i] > 0)
             es3_functions.glDrawArrays(mode, first[i], count[i]);
@@ -19,28 +28,31 @@ void glMultiDrawArrays( GLenum mode, GLint *first, GLsizei *count, GLsizei primc
 
 void glMultiDrawElements( GLenum mode, GLsizei *count, GLenum type, const void * const *indices, GLsizei primcount )
 {
-    if(!current_context) return;
+    if(!current_context || primcount == 0) return;
     GLint elementbuffer;
     es3_functions.glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &elementbuffer);
-    es3_functions.glBindBuffer(GL_COPY_WRITE_BUFFER, current_context->multidraw_element_buffer);
     GLsizei total = 0, offset = 0, typebytes = type_bytes(type);
     for (GLsizei i = 0; i < primcount; i++) {
         total += count[i];
     }
-    es3_functions.glBufferData(GL_COPY_WRITE_BUFFER, total*typebytes, NULL, GL_STREAM_DRAW);
+    if(total == 0) return;
+
+    bind_buffer_if_needed(GL_COPY_WRITE_BUFFER, current_context->multidraw_element_buffer);
+    es3_functions.glBufferData(GL_COPY_WRITE_BUFFER, total * typebytes, NULL, GL_STREAM_DRAW);
     for (GLsizei i = 0; i < primcount; i++) {
         GLsizei icount = count[i];
         if(icount == 0) continue;
         icount *= typebytes;
         if(elementbuffer != 0) {
             es3_functions.glCopyBufferSubData(GL_ELEMENT_ARRAY_BUFFER, GL_COPY_WRITE_BUFFER, (GLintptr)indices[i], offset, icount);
-        }else {
+        } else {
             es3_functions.glBufferSubData(GL_COPY_WRITE_BUFFER, offset, icount, indices[i]);
         }
         offset += icount;
     }
-    es3_functions.glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, current_context->multidraw_element_buffer);
+    bind_buffer_if_needed(GL_ELEMENT_ARRAY_BUFFER, current_context->multidraw_element_buffer);
     es3_functions.glDrawElements(mode, total, type, 0);
-    if(elementbuffer != 0) es3_functions.glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-
+    if(elementbuffer != 0) {
+        bind_buffer_if_needed(GL_ELEMENT_ARRAY_BUFFER, (GLuint)elementbuffer);
+    }
 }
